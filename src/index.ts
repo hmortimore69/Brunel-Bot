738a652d8cac14241c3b6dc155347fe52f94665f
Harvey Mortimore <harvey999mortimore@gmail.com>
2024-04-29 10:12:15 +0100

    Update .gitignore and add Discord bot initialization in index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Logged in as ${readyClient.user?.tag}`);
+})
+
+client.login(process.env.DISCORD_TOKEN);
