cc397d4b70042512c009192134dba5c634125255
Harvey Mortimore <harvey999mortimore@gmail.com>
2024-05-02 09:15:49 +0100

    changed png to svg

--- commands/utility/settings.ts
+++ commands/utility/settings.ts
@@ -10,12 +10,12 @@ module.exports = {
     .setDescription("Shows the settings for the bot"),
   async execute(interaction: CommandInteraction) {
     const embed = new EmbedBuilder()
-    .setTitle("Settings")
-        .setThumbnail("https://raw.githubusercontent.com/hmortimore69/Brunel-Bot/main/assets/images/gear.png")
-    .setColor("#00b0f4")
-    .setFooter({
-      text: "Designed by Harveyyyy",
-    })
+      .setTitle("Settings")
+      .setThumbnail("https://raw.githubusercontent.com/hmortimore69/Brunel-Bot/main/assets/images/gear.svg", )
+      .setColor("#00b0f4")
+      .setFooter({
+        text: "Designed by Harveyyyy",
+      })
     .setTimestamp();
 
     await interaction.reply({ embeds: [embed] });
